e8945eca4e7fa636a1e4047e3c12301aed6bcf95
Qizhi Zhang <qzhang@KBaseMacPro-QZ.local>
2019-09-26 22:26:14 -0500

    first prototype commit

---
+++ test/GenomeReport_server_test.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import unittest
+from configparser import ConfigParser
+
+from GenomeReport.GenomeReportImpl import GenomeReport
+from GenomeReport.GenomeReportServer import MethodContext
+from GenomeReport.authclient import KBaseAuth as _KBaseAuth
+
+from installed_clients.WorkspaceClient import Workspace
+
+
+class GenomeReportTest(unittest.TestCase):
+    """
+    Testing functions defined in GenomeReport
+    """
+    @classmethod
+    def setUpClass(cls):
+        token = os.environ.get('KB_AUTH_TOKEN', None)
+        config_file = os.environ.get('KB_DEPLOYMENT_CONFIG', None)
+        cls.cfg = {}
+        config = ConfigParser()
+        config.read(config_file)
+        for nameval in config.items('GenomeReport'):
+            cls.cfg[nameval[0]] = nameval[1]
+        # Getting username from Auth profile for token
+        authServiceUrl = cls.cfg['auth-service-url']
+        auth_client = _KBaseAuth(authServiceUrl)
+        user_id = auth_client.get_user(token)
+        # WARNING: don't call any logging methods on the context object,
+        # it'll result in a NoneType error
+        cls.ctx = MethodContext(None)
+        cls.ctx.update({'token': token,
+                        'user_id': user_id,
+                        'provenance': [
+                            {'service': 'GenomeReport',
+                             'method': 'please_never_use_it_in_production',
+                             'method_params': []
+                             }],
+                        'authenticated': 1})
+        cls.wsURL = cls.cfg['workspace-url']
+        cls.wsClient = Workspace(cls.wsURL)
+        cls.serviceImpl = GenomeReport(cls.cfg)
+        cls.scratch = cls.cfg['scratch']
+        cls.callback_url = os.environ['SDK_CALLBACK_URL']
+        suffix = int(time.time() * 1000)
+        cls.wsName = "test_GenomeReport_" + str(suffix)
+        ret = cls.wsClient.create_workspace({'workspace': cls.wsName})  # noqa
+
+    @classmethod
+    def tearDownClass(cls):
+        if hasattr(cls, 'wsName'):
+            cls.wsClient.delete_workspace({'workspace': cls.wsName})
+            print('Test workspace was deleted')
+
+    # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
+    def test_create_genome_report(self):
+        # Prepare test objects in workspace if needed using
+        # self.getWsClient().save_objects({'workspace': self.getWsName(),
+        #                                  'objects': []})
+        #
+        # Run your method by
+        # ret = self.getImpl().your_method(self.getContext(), parameters...)
+        #
+        # Check returned data with
+        # self.assertEqual(ret[...], ...) or other unittest methods
+        ret = self.serviceImpl.create_genome_report(self.ctx, {'output_workspace': self.wsName,
+                                                               'object_ref': '15792/185774/1',
+                                                               'annotated_by': 'Prokka'})
